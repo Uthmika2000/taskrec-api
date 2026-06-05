b205320e57c951337a7400a6dc4cdf106b042113
hiru2k <mudannayakehiruni90@gmail.com>
2026-06-05 14:34:49 +0530

    feat(teams): add description field to Team model

--- server/models/Team.js
+++ server/models/Team.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
+    description: { type: String, default: '', trim: true },
     adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     memberIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     sprintIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Sprint' }],
